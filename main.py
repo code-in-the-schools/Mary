afedc90e4f6175e49c8ad49a1f3315dcc0f2b3fa
Mary Brown <marycbrown2004@gmail.com>
2020-07-16 16:05:47 +0000

    fixed it

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-a = 0
-b = 0
+userInput = str(input("Give me a character: "))
 
-a = input("Give me number a")
-b = input("Give me number b")
-c = int(a) + int(b)
+d_1 = "    " + userInput
+d_2 = "   " + userInput + " " + userInput
+d_3 = "  " + userInput + "   " + userInput
+d_4 = " " + userInput + "     " + userInput
+d_5 = userInput + "       " + userInput
 
-print (c)
-#A basic calculator
+print(d_1)
+print(d_2)
+print(d_3)
+print(d_4)
+print(d_5)
+print(d_4)
+print(d_3)
+print(d_2)
+print(d_1)
+#Wanted to do the calculator thing to be extra but couldn't get it to work
